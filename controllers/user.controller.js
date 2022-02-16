2f1e5e10e6d3bb0488da0bb1f1950ee28de2a67f
mahesh gandharw <mahesh.thoughtwin@gmail.com>
2022-02-16 17:58:29 +0530

    referenceTokenGenerate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,9 @@
 const db = require("../models");
 const bcrypt = require("bcrypt");
-// require("dotenv").config()
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 const { createEmailSender } = require("./emailSender")
 const User = db.users;
-// const Op = db.Sequelize.Op;
 
 
 exports.createUser = async (req, res) => {
@@ -26,8 +25,8 @@ exports.createUser = async (req, res) => {
     const subject = "Registered User App";
     User.create(user)
         .then(data => {
-            res.status(201).json({ data: data,msg:success})
-            createEmailSender(email,subject,success);
+            res.status(201).json({ data: data, msg: success })
+            createEmailSender(email, subject, success);
         })
         .catch(err => {
             res.status(500).send({
@@ -47,10 +46,11 @@ exports.loginUser = async (req, res) => {
             id: user.id,
             email: user.email
         }
-        const token = await jwt.sign(data, "mynameismaheshnodejsdevloper");
-        console.log(token)
+        const access_token = await jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "8h" });
+        const refresh_token = await jwt.sign({ data, refresh: true }, process.env.JWT_SECRET, { expiresIn: "30 days" });
+        // console.log(token)
         if (isMatch) {
-            res.status(200).json({ email: user.email, password: user.password, auth_token: token, msg: "User LoggedIn!" });
+            res.status(200).json({ email: user.email, password: user.password, access_token, refresh_token, msg: "User LoggedIn!" });
         } else {
             res.status(404).json({ error: 'Invalid password Details' });
         }
@@ -63,7 +63,7 @@ exports.changePasswordUser = async (req, res) => {
     try {
         let token = req.headers.authorization;
         let onlyToken = token.split(' ')[1]
-        var decoded = jwt.verify(onlyToken, "mynameismaheshnodejsdevloper");
+        var decoded = jwt.verify(onlyToken, process.env.JWT_SECRET);
         var userId = decoded.id
         const salt = await bcrypt.genSalt(8);
         const password = await bcrypt.hash(req.body.password, salt);
@@ -72,7 +72,7 @@ exports.changePasswordUser = async (req, res) => {
         const id = await User.findOne({ where: { id: userId } }).then((user) => {
             user.update({ password: password }).then((user) => {
                 res.status(200).json({ data: user, msg: "Changed password successfully" })
-                createEmailSender(decoded.email,subject,success)
+                createEmailSender(decoded.email, subject, success)
             })
         });
     } catch (err) {
@@ -94,7 +94,7 @@ exports.updateProfileUsers = async (req, res) => {
         const { first_name, last_name, email, gender, password } = req.body;
         let token = req.headers.authorization;
         let onlyToken = token.split(' ')[1]
-        var decoded = jwt.verify(onlyToken, "mynameismaheshnodejsdevloper");
+        var decoded = jwt.verify(onlyToken, process.env.JWT_SECRET);
         var userId = decoded.id
         const encryptedPassword = await bcrypt.hash(password, 8);
         const success = "Profile Updated successfully!";
@@ -103,12 +103,12 @@ exports.updateProfileUsers = async (req, res) => {
             user.update({
                 first_name: first_name,
                 last_name: last_name,
-                email: email,
+                // email: email,
                 gender: gender,
                 password: encryptedPassword
             }).then((user) => {
                 res.status(200).json({ data: user, msg: "Profile Updated successfully" })
-                createEmailSender(decoded.email,subject,success)
+                createEmailSender(decoded.email, subject, success)
             })
         })
     } catch (err) {
@@ -120,7 +120,7 @@ exports.deleteUserDetails = async (req, res) => {
     try {
         let token = req.headers.authorization;
         let onlyToken = token.split(' ')[1]
-        var decoded = jwt.verify(onlyToken, "mynameismaheshnodejsdevloper");
+        var decoded = jwt.verify(onlyToken, process.env.JWT_SECRET);
         var userId = decoded.id
         let user = await User.destroy({ where: { id: userId } }).then((user) => {
             if (user) {
--- expressApp.js
+++ expressApp.js
@@ -3,12 +3,9 @@ const cors = require("cors");
 const db = require("./models");
 const userRouter = require("./routes/user.routes");
 const app = express();
+require('dotenv').config();
 
-var corsOptions = {
-    origin: "http://localhost:4005"
-}
 
-app.use(cors(corsOptions))
 
 
 app.use(express.urlencoded({ extended: false }));
@@ -17,7 +14,7 @@ db.sequelize.sync();
 
 app.use('/api/users',userRouter);
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORTADD || 5000
 
 app.listen(PORT, (err) => {
     console.log(`Server is running on port ${PORT}`);
